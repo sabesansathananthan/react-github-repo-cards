da0e57ca17750b3094340d1c31b2cd177f88b504
Sathananthan Sabesan <36799589+sabesansathananthan@users.noreply.github.com>
2020-10-03 15:36:29 +0530

    Deploy (#5)

--- src/components/GitHubCards.js
+++ src/components/GitHubCards.js
@@ -38,7 +38,7 @@ class GitHubCards extends Component {
       async (url) =>
         await Axios.get(url, {
           headers: {
-            Authorization: `token ${process.env.GitHubApiKey}`,
+            Authorization: `token ${process.env.GITHUB_API_KEY}`,
           },
         }).then(async (res) => {
           await this.setState({
